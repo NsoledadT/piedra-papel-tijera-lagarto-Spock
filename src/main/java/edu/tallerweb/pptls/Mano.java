eff15df38daa07a85efa67639434989c18c9124f
soledadt2013 <soledadt2013@gmail.com>
2013-09-19 18:04:51 -0300

    Archivo Final con las Firmas Intactas y sin If

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,30 +4,18 @@ package edu.tallerweb.pptls;
 public class Mano {
 	
     private final Integer forma;
-    private int resultadoJuego;
-    private final int tablaResultado[][] = {{0,1,1,2,2},
-			                                {2,0,1,1,2},
-			                                {2,2,0,1,1},
-			                                {1,2,2,0,1},
-			                                {1,1,2,2,0}};
+    private final Resultado tablaResultado[][] = {{Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA,Resultado.GANA},
+			                                      {Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA},
+			                                      {Resultado.GANA,Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE},
+			                                      {Resultado.PIERDE,Resultado.GANA,Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE},
+			                                      {Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA,Resultado.GANA,Resultado.EMPATA}};
 	public Mano( final Forma forma) {
 		this.forma=forma.getValor();
-	}
+	  }
 	
 	public Resultado jugarCon(final Mano otra) {
-	   resultadoJuego = tablaResultado[this.forma][otra.forma];
-	   if(resultadoJuego == 2){
-		     return Resultado.GANA;
-	    }
-	   else{
-	      if(resultadoJuego == 1){
-			 return Resultado.PIERDE;
-			    }
-	    
-	      else{
-	    	 return Resultado.EMPATA;
-	 		    }
-	      }
-	 }
-	
+	    return tablaResultado[this.forma][otra.forma];
+	  }
 }
+	
+
--- src/test/java/edu/tallerweb/pptls/PartidaTests.java
+++ src/test/java/edu/tallerweb/pptls/PartidaTests.java
@@ -8,11 +8,31 @@ public class PartidaTests {
 	@Test
 	public void queSpockVaporizaPiedra() {
 
-		Mano jugadorUno = new Mano(Forma.PIEDRA);
+		Mano jugadorUno = new Mano(Forma.SPOCK);
+		Mano jugadorDos = new Mano(Forma.PIEDRA);
+
+		assertEquals("Spock vaporiza Piedra",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+	}
+		@Test
+		public void queTijeraDecapitaLagarto() {
+		
+		Mano jugadorUno = new Mano(Forma.LAGARTO);
 		Mano jugadorDos = new Mano(Forma.TIJERA);
 
-		assertEquals("Piedra aplasta Tijera",
-				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+			assertEquals("Tijera Decapita Lagarto",
+					Resultado.PIERDE, jugadorUno.jugarCon(jugadorDos));
+	
+     }
+		
+		@Test
+		public void quePapelEnvuelvePapel() {
+		
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.PAPEL);
+
+			assertEquals("Papel Envuelve Papel",
+					Resultado.EMPATA, jugadorUno.jugarCon(jugadorDos));
 	
      }
 
