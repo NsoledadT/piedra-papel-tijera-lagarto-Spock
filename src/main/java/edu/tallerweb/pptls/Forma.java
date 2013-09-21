84a68768b6378c5c575b4d0394fcd7506759adcd
soledadt2013 <soledadt2013@gmail.com>
2013-09-21 09:46:48 -0300

    Otra con correciones al estilo

--- src/main/java/edu/tallerweb/pptls/Forma.java
+++ src/main/java/edu/tallerweb/pptls/Forma.java
@@ -3,18 +3,17 @@ package edu.tallerweb.pptls;
 /**
  * Las diferentes Formas que existen en el juego
  */
-public enum Forma {
+public enum Forma{
 
 	PIEDRA(0), SPOCK(1), PAPEL(2), LAGARTO(3), TIJERA(4);
 
 	private  final Integer valor;
 
-	private Forma( final Integer valor) {
+	private Forma( final Integer valor){
 		this.valor = valor;
 	}
 
-	public Integer getValor() {
+	public Integer getValor(){
 		return this.valor;
 	}
-
 }
--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,19 +1,19 @@
 package edu.tallerweb.pptls;
 
 
-public class Mano {
+public class Mano{
 	
     private final Integer forma;
-    private final Resultado tablaResultado[][] = {{Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA,Resultado.GANA},
-			                                      {Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA},
-			                                      {Resultado.GANA,Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE,Resultado.PIERDE},
-			                                      {Resultado.PIERDE,Resultado.GANA,Resultado.GANA,Resultado.EMPATA,Resultado.PIERDE},
-			                                      {Resultado.PIERDE,Resultado.PIERDE,Resultado.GANA,Resultado.GANA,Resultado.EMPATA}};
-	public Mano( final Forma forma) {
-		this.forma=forma.getValor();
+    private Resultado tablaResultado[][]={{Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE, Resultado.GANA, Resultado.GANA},
+			                              {Resultado.GANA, Resultado.EMPATA,Resultado.PIERDE, Resultado.PIERDE, Resultado.GANA},
+			                              {Resultado.GANA, Resultado.GANA,Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE},
+			                              {Resultado.PIERDE, Resultado.GANA,Resultado.GANA, Resultado.EMPATA, Resultado.PIERDE},
+			                              {Resultado.PIERDE, Resultado.PIERDE,Resultado.GANA, Resultado.GANA, Resultado.EMPATA}};
+	public Mano( final Forma forma){
+		this.forma = forma.getValor();
 	  }
 	
-	public Resultado jugarCon(final Mano otra) {
+	public Resultado jugarCon(final Mano otra){
 	    return tablaResultado[this.forma][otra.forma];
 	  }
 }
