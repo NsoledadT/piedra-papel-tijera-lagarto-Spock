137d56dd10c9259fdc5a6809165f23ba708984a2
soledadt2013 <soledadt2013@gmail.com>
2013-09-27 15:39:51 -0300

    Eliminar espacios y otros

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,11 +4,11 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-	
+
     private final Integer forma;
     private int variable;
-    private final Resultado resultado[] = {Resultado.EMPATA, Resultado.PIERDE, Resultado.GANA};
-    private final int tablaResultado[][] = {{0, 1, 1, 2, 2},
+    private final Resultado [] resultado = {Resultado.EMPATA, Resultado.PIERDE, Resultado.GANA};
+    private final int [][] tablaResultado = {{0, 1, 1, 2, 2},
     		                                {2, 0, 1, 1, 2},
     		                                {2, 2, 0, 1, 1},
     		                                {1, 2, 2, 0, 1},
@@ -16,7 +16,7 @@ public class Mano {
     public Mano(final Forma forma) {
 		this.forma = forma.getValor();
 	  }
-	
+
     public Resultado jugarCon(final Mano otra) {
         variable = tablaResultado[this.forma][otra.forma];
          return resultado[variable];
