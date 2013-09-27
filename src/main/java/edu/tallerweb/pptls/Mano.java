c8b95775cfa2356154fd54a3fb2da147fd96a822
soledadt2013 <soledadt2013@gmail.com>
2013-09-27 11:36:58 -0300

    Cambios en espacios en blanco

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -5,22 +5,18 @@ public class Mano{
 	
     private final Integer forma;
     private int variable;
-    private final int tablaResultado[][] = {{0,1,1,2,2},
-    	                                    {2,0,1,1,2},
-                                            {2,2,0,1,1},   
-                                            {1,2,2,0,1},  
-                                            {1,1,2,2,0}};
-    
-    private final Resultado Vector[] = {Resultado.EMPATA,Resultado.PIERDE,Resultado.GANA};
-	
-    public Mano( final Forma forma){
+    private final Resultado Vector[] = {Resultado.EMPATA, Resultado.PIERDE, Resultado.GANA};
+    private final int tablaResultado[][] = {{0, 1, 1, 2, 2},
+    		                                {2, 0, 1, 1, 2},
+    		                                {2, 2, 0, 1, 1},
+    		                                {1, 2, 2, 0, 1},
+    		                                {1, 1, 2, 2, 0}};
+    public Mano( final Forma forma ) {
 		this.forma = forma.getValor();
 	  }
 	
-    public Resultado jugarCon(final Mano otra){
-    variable=tablaResultado[this.forma][otra.forma];
-    return Vector[variable];
-	   
+    public Resultado jugarCon( final Mano otra ) {
+        variable=tablaResultado[this.forma][otra.forma];
+         return Vector[variable];
 	  }
 }
-	
