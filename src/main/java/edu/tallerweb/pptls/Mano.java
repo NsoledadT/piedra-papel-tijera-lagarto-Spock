a653449524ca9909b365f9b1babd3a25ef65e08e
soledadt2013 <soledadt2013@gmail.com>
2013-09-27 11:22:15 -0300

    Algunos cambios más

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,18 +4,23 @@ package edu.tallerweb.pptls;
 public class Mano{
 	
     private final Integer forma;
-    private Resultado tablaResultado[][]={{Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE, Resultado.GANA, Resultado.GANA},
-			                              {Resultado.GANA, Resultado.EMPATA,Resultado.PIERDE, Resultado.PIERDE, Resultado.GANA},
-			                              {Resultado.GANA, Resultado.GANA,Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE},
-			                              {Resultado.PIERDE, Resultado.GANA,Resultado.GANA, Resultado.EMPATA, Resultado.PIERDE},
-			                              {Resultado.PIERDE, Resultado.PIERDE,Resultado.GANA, Resultado.GANA, Resultado.EMPATA}};
-	public Mano( final Forma forma){
+    private int variable;
+    private final int tablaResultado[][] = {{0,1,1,2,2},
+    	                                    {2,0,1,1,2},
+                                            {2,2,0,1,1},   
+                                            {1,2,2,0,1},  
+                                            {1,1,2,2,0}};
+    
+    private final Resultado Vector[] = {Resultado.EMPATA,Resultado.PIERDE,Resultado.GANA};
+	
+    public Mano( final Forma forma){
 		this.forma = forma.getValor();
 	  }
 	
-	public Resultado jugarCon(final Mano otra){
-	    return tablaResultado[this.forma][otra.forma];
+    public Resultado jugarCon(final Mano otra){
+    variable=tablaResultado[this.forma][otra.forma];
+    return Vector[variable];
+	   
 	  }
 }
 	
-
