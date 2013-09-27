f77bcc6d9975fb88e91d397ab66023752d23603b
soledadt2013 <soledadt2013@gmail.com>
2013-09-27 11:55:04 -0300

    Espacios en blanco de mas cambio de nombres

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,22 +1,22 @@
 package edu.tallerweb.pptls;
 
 
-public class Mano{
+public class Mano {
 	
     private final Integer forma;
     private int variable;
-    private final Resultado Vector[] = {Resultado.EMPATA, Resultado.PIERDE, Resultado.GANA};
+    private final Resultado resultado[] = {Resultado.EMPATA, Resultado.PIERDE, Resultado.GANA};
     private final int tablaResultado[][] = {{0, 1, 1, 2, 2},
     		                                {2, 0, 1, 1, 2},
     		                                {2, 2, 0, 1, 1},
     		                                {1, 2, 2, 0, 1},
     		                                {1, 1, 2, 2, 0}};
-    public Mano( final Forma forma ) {
+    public Mano(final Forma forma) {
 		this.forma = forma.getValor();
 	  }
 	
-    public Resultado jugarCon( final Mano otra ) {
-        variable=tablaResultado[this.forma][otra.forma];
-         return Vector[variable];
+    public Resultado jugarCon(final Mano otra) {
+        variable = tablaResultado[this.forma][otra.forma];
+         return resultado[variable];
 	  }
 }
